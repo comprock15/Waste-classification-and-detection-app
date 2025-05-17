417c6b086bfc548e180f8ecb1a70b15495af171e
comprock15 <khoruzhaia@sfedu.ru>
2025-05-18 01:29:53 +0300

    Require permissions

--- WasteDetectorApp/app/src/main/java/com/example/wastedetector/MainActivity.kt
+++ WasteDetectorApp/app/src/main/java/com/example/wastedetector/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.wastedetector
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val PERMISSIONS_REQUIRED = arrayOf(android.Manifest.permission.CAMERA)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,39 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            requestPermissions()
+        }
+
+    }
+
+    private fun startCamera() {
+        TODO("Not yet implemented")
+    }
+
+
+    // ----------------- Permissions ---------------------
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(this, PERMISSIONS_REQUIRED, 1)
+    }
+
+    private fun allPermissionsGranted() = PERMISSIONS_REQUIRED.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (!grantResults.all { it == PackageManager.PERMISSION_GRANTED}) {
+            requestPermissions()
         }
     }
 }

111f96bceeade7032f74af1f971121ace9abda4a
comprock15 <khoruzhaia@sfedu.ru>
2025-05-29 00:19:03 +0300

    Add object detection

--- WasteDetectorApp/app/src/main/java/com/example/wastedetector/MainActivity.kt
+++ WasteDetectorApp/app/src/main/java/com/example/wastedetector/MainActivity.kt
@@ -1,39 +1,52 @@
 package com.example.wastedetector
 
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.os.Bundle
 import android.util.Log
+import android.view.Surface
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
-import androidx.camera.view.PreviewView
+import androidx.camera.core.resolutionselector.AspectRatioStrategy
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.graphics.createBitmap
+import com.example.wastedetector.Constants.LABELS_PATH
+import com.example.wastedetector.Constants.MODEL_PATH
+import com.example.wastedetector.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var previewView: PreviewView
+class MainActivity : AppCompatActivity(), Detector.DetectorListener {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var bitmapBuffer: Bitmap
+
+    private var detector: Detector? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        previewView = findViewById(R.id.previewView)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Request camera permissions
         if (!allPermissionsGranted()) {
             requestPermissions()
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        detector = Detector(baseContext, MODEL_PATH, LABELS_PATH, this) {
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         }
 
         startCamera()
@@ -46,27 +59,78 @@ class MainActivity : AppCompatActivity() {
         cameraProviderFuture.addListener({
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
-            val preview = Preview.Builder().build().also {
-                it.surfaceProvider = previewView.surfaceProvider
-            }
+            var resolutionSelector = ResolutionSelector.Builder()
+                .setAspectRatioStrategy(AspectRatioStrategy.RATIO_4_3_FALLBACK_AUTO_STRATEGY)
+                .build()
 
-            val imageAnalysis = ImageAnalysis.Builder()
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            // TODO: handle rotations
+            //val rotation = binding.main.display.rotation
+            val rotation = Surface.ROTATION_0
+
+            // Setup camera preview
+            val preview = Preview.Builder()
+                .setResolutionSelector(resolutionSelector)
+                .setTargetRotation(rotation)
                 .build()
 
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
+            val imageAnalysis = ImageAnalysis.Builder()
+                .setResolutionSelector(resolutionSelector)
+                .setTargetRotation(rotation)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .build()
+
+            imageAnalysis.setAnalyzer(cameraExecutor) { image ->
+                if (!::bitmapBuffer.isInitialized) {
+                    bitmapBuffer = createBitmap(image.width, image.height)
+                    }
+                image.use { bitmapBuffer.copyPixelsFromBuffer(image.planes[0].buffer) }
+                image.close()
+
+                val matrix = Matrix().apply {
+                    postRotate(image.imageInfo.rotationDegrees.toFloat())
+                }
+
+                val rotatedBitmap = Bitmap.createBitmap(
+                    bitmapBuffer, 0, 0, bitmapBuffer.width, bitmapBuffer.height,
+                    matrix, true
+                )
+
+                detector?.detect(rotatedBitmap)
+            }
+
+            cameraProvider.unbindAll()
             try {
-                cameraProvider.unbindAll()
                 cameraProvider.bindToLifecycle(
                     this, cameraSelector, preview, imageAnalysis
                 )
+                preview.surfaceProvider = binding.previewView.surfaceProvider
             } catch(exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
         }, ContextCompat.getMainExecutor(this))
     }
 
+    override fun onEmptyDetect() {
+        binding.overlayView.clear()
+    }
+
+    override fun onDetect(boundingBoxes: List<BoundingBox>, inferenceTime: Long) {
+        runOnUiThread {
+            binding.overlayView.apply {
+                setResults(boundingBoxes)
+                invalidate()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        detector?.close()
+        cameraExecutor.shutdown()
+    }
 
     // ----------------- Permissions ---------------------
     private fun requestPermissions() {

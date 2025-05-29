d0f33d42bee14f8897a521941df1e9ae513b2312
comprock15 <khoruzhaia@sfedu.ru>
2025-05-29 18:43:53 +0300

    Refactor MainActivity.kt

--- WasteDetectorApp/app/src/main/java/com/example/wastedetector/MainActivity.kt
+++ WasteDetectorApp/app/src/main/java/com/example/wastedetector/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.core.resolutionselector.AspectRatioStrategy
 import androidx.camera.core.resolutionselector.ResolutionSelector
@@ -31,19 +32,51 @@ class MainActivity : AppCompatActivity(), Detector.DetectorListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        initializeUI()
+        checkCameraPermissions()
+        initializeComponents()
+    }
 
+    private fun initializeUI() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
 
-        // Request camera permissions
+    // ----------------- Permissions ---------------------
+
+    private fun checkCameraPermissions() {
         if (!allPermissionsGranted()) {
             requestPermissions()
         }
+    }
 
+    private fun allPermissionsGranted() = PERMISSIONS_REQUIRED.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(this, PERMISSIONS_REQUIRED, REQUEST_CODE_PERMISSIONS)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                Toast.makeText(this, "Permissions not granted", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    // ----------------- Setup camera ---------------------
+
+    private fun initializeComponents() {
         cameraExecutor = Executors.newSingleThreadExecutor()
-
         detector = Detector(baseContext, MODEL, LABELS, this)
-
         startCamera()
     }
 
@@ -53,59 +86,79 @@ class MainActivity : AppCompatActivity(), Detector.DetectorListener {
         // Bind camera use cases
         cameraProviderFuture.addListener({
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            var resolutionSelector = ResolutionSelector.Builder()
-                .setAspectRatioStrategy(AspectRatioStrategy.RATIO_4_3_FALLBACK_AUTO_STRATEGY)
-                .build()
-
-            // TODO: handle rotations
-            //val rotation = binding.main.display.rotation
-            val rotation = Surface.ROTATION_0
-
-            // Setup camera preview
-            val preview = Preview.Builder()
-                .setResolutionSelector(resolutionSelector)
-                .setTargetRotation(rotation)
-                .build()
-
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            val imageAnalysis = ImageAnalysis.Builder()
-                .setResolutionSelector(resolutionSelector)
-                .setTargetRotation(rotation)
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
-                .build()
-
-            imageAnalysis.setAnalyzer(cameraExecutor) { image ->
-                if (!::bitmapBuffer.isInitialized) {
-                    bitmapBuffer = createBitmap(image.width, image.height)
-                    }
-                image.use { bitmapBuffer.copyPixelsFromBuffer(image.planes[0].buffer) }
-                image.close()
-
-                val matrix = Matrix().apply {
-                    postRotate(image.imageInfo.rotationDegrees.toFloat())
-                }
-
-                val rotatedBitmap = Bitmap.createBitmap(
-                    bitmapBuffer, 0, 0, bitmapBuffer.width, bitmapBuffer.height,
-                    matrix, true
-                )
-
-                detector?.detect(rotatedBitmap)
-            }
-
-            cameraProvider.unbindAll()
-            try {
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageAnalysis
-                )
-                preview.surfaceProvider = binding.previewView.surfaceProvider
-            } catch(exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
+            setupCamera(cameraProvider)
         }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun setupCamera(cameraProvider: ProcessCameraProvider) {
+        var resolutionSelector = createResolutionSelector()
+        val rotation = getRotation()
+
+        val preview = createPreview(resolutionSelector, rotation)
+        val imageAnalysis = createImageAnalysis(resolutionSelector, rotation)
+
+        try {
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(
+                this, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageAnalysis
+            )
+            preview.surfaceProvider = binding.previewView.surfaceProvider
+        } catch(exc: Exception) {
+            Log.e(TAG, "Use case binding failed", exc)
+        }
+    }
+
+    private fun createPreview(
+        resolutionSelector: ResolutionSelector,
+        rotation: Int
+    ): Preview = Preview.Builder()
+        .setResolutionSelector(resolutionSelector)
+        .setTargetRotation(rotation)
+        .build()
+
+    // TODO: handle rotations
+    private fun getRotation(): Int = Surface.ROTATION_0 // binding.main.display.rotation
+
+    private fun createResolutionSelector(
+        strategy: AspectRatioStrategy = AspectRatioStrategy.RATIO_4_3_FALLBACK_AUTO_STRATEGY
+    ): ResolutionSelector = ResolutionSelector.Builder()
+        .setAspectRatioStrategy(strategy)
+        .build()
+
+    private fun createImageAnalysis(
+        resolutionSelector: ResolutionSelector,
+        rotation: Int
+    ): ImageAnalysis = ImageAnalysis.Builder()
+        .setResolutionSelector(resolutionSelector)
+        .setTargetRotation(rotation)
+        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+        .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+        .build()
+        .apply { setAnalyzer(cameraExecutor, createAnalyzer()) }
+
+    private fun createAnalyzer() = ImageAnalysis.Analyzer { image ->
+        if (!::bitmapBuffer.isInitialized) {
+            bitmapBuffer = createBitmap(image.width, image.height)
+        }
+
+        processCameraImage(image).let { processedImage ->
+            detector.detect(processedImage)
+        }
+    }
+
+    private fun processCameraImage(image: ImageProxy): Bitmap {
+        return image.use { img ->
+            bitmapBuffer.copyPixelsFromBuffer(img.planes[0].buffer)
+            Bitmap.createBitmap(
+                bitmapBuffer, 0, 0,
+                bitmapBuffer.width,
+                bitmapBuffer.height,
+                Matrix().apply {
+                    postRotate(image.imageInfo.rotationDegrees.toFloat())
+                },
+                true
+            )
+        }
     }
 
     override fun onEmptyDetect() {
@@ -125,29 +178,6 @@ class MainActivity : AppCompatActivity(), Detector.DetectorListener {
         super.onDestroy()
         detector.close()
         cameraExecutor.shutdown()
-    }
-
-    // ----------------- Permissions ---------------------
-    private fun requestPermissions() {
-        ActivityCompat.requestPermissions(this, PERMISSIONS_REQUIRED, REQUEST_CODE_PERMISSIONS)
-    }
-
-    private fun allPermissionsGranted() = PERMISSIONS_REQUIRED.all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (!grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
-                Toast.makeText(this, "Permissions not granted", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }
     }
 
     companion object {

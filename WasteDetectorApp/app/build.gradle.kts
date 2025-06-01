7e325214785328507cbb4dd9e4bf6bca604746b2
comprock15 <khoruzhaia@sfedu.ru>
2025-06-01 16:40:21 +0300

    Increment versionCode

--- WasteDetectorApp/app/build.gradle.kts
+++ WasteDetectorApp/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.simpledimploma.wastedetector"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "v1.0.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
